c50fe7317fb7179bcee3f8ec243c7f2f0b766214
SaharBarak <sahar.h.barak@gmail.com>
2024-01-16 17:28:36 +0200

    add rate limiter

--- app.ts
+++ app.ts
@@ -6,15 +6,15 @@ import rateLimit from 'express-rate-limit';
 const cors = require("cors");
 const app = express();
 
-// // Define rate limit settings
-// const apiLimiter = rateLimit({
-//   windowMs: 15 * 60 * 1000, // 15 minutes
-//   max: 1, // Limit each IP to 100 requests per windowMs
-//   message: 'Too many requests from this IP, please try again after 15 minutes'
-// });
+// Define rate limit settings
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 15, // Limit each IP to 100 requests per windowMs
+  message: 'Too many requests from this IP, please try again after 15 minutes'
+});
 
-// // Apply the rate limit to all requests
-// app.use(apiLimiter);
+// Apply the rate limit to all requests
+app.use(apiLimiter);
 app.use(express.json());
 
 console.log("connecting to database");
